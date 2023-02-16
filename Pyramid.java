c84f566463429491d3fc1a5d41ddb0098f9afd5d
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-02-16 23:12:34 +0530

    java files

---
+++ Pyramid.java
@@ -0,0 +1,28 @@
+public class Pyramid {
+
+/* public static void printStars(int n) {
+int i,j;
+for(i=0;i<n;i++){
+for(j=0;j<=i;j++) {
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+public static void main(String args[]) {
+int n=5;
+printStars(n);
+}  */
+
+
+public static void main(String args[]) {
+java.util.Scanner sc=new java.util.Scanner(System.in);
+int n=sc.nextInt();
+for(int i=0;i<n;i++){
+for(int j=0;j<=i;j++) {
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
---
+++ RevPyramid.java
@@ -0,0 +1,15 @@
+public class RevPyramid {
+public static void main(String args[]) {
+for(int i=5;i>0;i--){
+
+/* for(int j=5;j>=i;j--){
+System.out.print(j+" ");
+} */
+
+for(int j=0;j<i;j++){
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
