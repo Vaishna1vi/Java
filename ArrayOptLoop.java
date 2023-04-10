2bc451e94c5eff6f315b3ba7cb6c4c66ccb4c987
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-04-10 23:35:19 +0530

    modification in array optimized loop

--- ArrayOptLoop.java
+++ ArrayOptLoop.java
@@ -7,16 +7,19 @@ public class ArrayOptLoop {
         // a[1]=3;
         // a[2]=4;
         // a[3]=5;
+        System.out.println("Enter the value of n: ");
         Scanner sc = new Scanner(System.in);
         // int n;
         int n=sc.nextInt();
         int[] a = new int[n];
+        System.out.println("Enter the elements: ");
 for(int i=0; i<n;i++) {
   a[i]= sc.nextInt();
 }
 sc.close();
-for(int x:a){
-    System.out.println(x);
-}
+System.out.println("The elements present in the array are: ");
+  for(int x:a){
+      System.out.print(x+"\t");
+    }
     }
 }
