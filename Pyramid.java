262675d38011e3c68e4447c86347d395418f1e71
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-04-11 00:12:06 +0530

    changes in some file and created start pattern reverse file

--- Pyramid.java
+++ Pyramid.java
@@ -1,6 +1,6 @@
 public class Pyramid {
 
-/* public static void printStars(int n) {
+public static void printStars(int n) {
 int i,j;
 for(i=0;i<n;i++){
 for(j=0;j<=i;j++) {
@@ -12,18 +12,20 @@ System.out.println();
 public static void main(String args[]) {
 int n=5;
 printStars(n);
-}  */
+}  
+}
 
-
-public static void main(String args[]) {
-java.util.Scanner sc=new java.util.Scanner(System.in);
-int n=sc.nextInt();
-sc.close();
-for(int i=0;i<n;i++){
-for(int j=0;j<=i;j++) {
-System.out.print("*");
-}
-System.out.println();
-}
-}
-}
+// public static void main(String args[]) {
+//     System.out.println("Enter the value of n: ");
+// java.util.Scanner sc=new java.util.Scanner(System.in);
+// int n=sc.nextInt();
+// sc.close();
+// System.out.println("Star pattern upto the value of n:- ");
+// for(int i=0;i<n;i++){
+// for(int j=0;j<=i;j++) {
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+// }
