1912fb7018d11ae453e00f5162d67490844a30b5
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-09-27 23:26:30 +0530

    created java pattern file

---
+++ javaPattern.java
@@ -0,0 +1,15 @@
+public class javaPattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for(int j=0; j<4; j++) {
+                if((i==3 && j==0)||(i==4 && j==2 && j!=3 && j==1) || j==3) {   
+                    System.out.print("J ");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
