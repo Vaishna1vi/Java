ad05daba99e8d9395e2448aaff389e54635889fd
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-09-28 23:57:11 +0530

    modified java pattern file

--- javaPattern.java
+++ javaPattern.java
@@ -1,15 +1,22 @@
 public class javaPattern {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for(int j=0; j<4; j++) {
-                if((i==3 && j==0)||(i==4 && j==2 && j!=3 && j==1) || j==3) {   
-                    System.out.print("J ");
-                }
-                else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println(" ");
+        // for (int i = 0; i < 5; i++) {
+        //     for(int j=0; j<4; j++) {
+        //         if((i==3 && j==0)||(i==4 && j==2 && j!=3 && j==1) || j==3) {   
+        //             System.out.print("J ");
+        //         }
+        //         else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(" ");
+
+        System.out.println("    J       A     V       V     A");
+        System.out.println("    J      A A     V     V     A A");
+        System.out.println("    J     A   A     V   V     A   A");
+        System.out.println("J   J    A A A A     V V     A A A A");
+        System.out.println(" J J    A       A     V     A       A");
+
         }
     }
 }
