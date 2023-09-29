e72f7b45458e3ddab6384afb15072686323185f3
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-09-29 23:04:13 +0530

    modified sumOfArrayElements file by alternative approach

--- sumOfArrayElements.java
+++ sumOfArrayElements.java
@@ -11,18 +11,21 @@ public class sumOfArrayElements {
         for (int i = 0; i < max; i++) {
             arr[i] = s.nextInt();
         }
-        
+
         System.out.print("The array is: ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
 
-        int sum = 0;
+        // int sum = 0;
+        int sum = arr[0]; // alternative way
         for (int i = 0; i < max; i++) {
             sum = sum + arr[i];
         }
-        System.out.print("The sum of the elements present in the array is: " + sum);
+        int ans = sum - arr[0]; // alternative way
+        // System.out.print("The sum of the elements present in the array is: " + sum);
+        System.out.print("The sum of the elements present in the array is: " + ans); // alternative way
         s.close();
     }
 }
