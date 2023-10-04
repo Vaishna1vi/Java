bb29ac465a7e6d08990180eff81d5eb4146f44c0
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-04 22:10:12 +0530

    modified sumSquareNum file

--- sumSquareNum.java
+++ sumSquareNum.java
@@ -6,18 +6,20 @@ public class sumSquareNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
-        int num = sc.nextInt();
-        int i = 0, n, sum = 0, m;
-        for (i = 0; i < 5; i++) {
-            n = num % 10;
-            m = num / 10;
-            sum = n  + sum;
-        }
-        System.out.println(sum);
-        if (i < 5) {
-        } else {
-            System.out.println("Please enter the less or equal to five digit number. ");
-        }
+        String str = sc.nextLine();
+        int num = Integer.valueOf(str);
         sc.close();
+
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            sum = sum + ((num % 10) * (num % 10));
+            num = num / 10;
+        }
+
+        if (str.length() <= 5) {
+            System.out.println("The sum of square of each digit of a number is: " + sum);
+        } else {
+            System.out.println("Please enter the less or equal to five digit number.");
+        }
     }
 }
