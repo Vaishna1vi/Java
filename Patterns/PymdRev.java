7888e7be5c0d0e77e8e89f88c24de7269a822812
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-06 16:39:43 +0530

    modified file with another approach to print reverse pyramid pattern

--- Patterns/PymdRev.java
+++ Patterns/PymdRev.java
@@ -1,9 +1,10 @@
 public class PymdRev {
     public static void main(String[] args) {
-        for(int i=5;i>=0;i--) {
-            // for(int i=0; i<5; i++) {
-            for(int j=0;j<i; j++) {
-                // for(int j=i; j>=0; j--) {
+        int n = 5;
+        // for(int i=5;i>=0;i--) {
+        for (int i = 0; i < n; i++) {
+            // for(int j=0;j<i; j++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
