eafd2729741b4136eb01186cb7833208b7840727
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-08 20:01:09 +0530

    demonstrated the loops concept to run infinity times

---
+++ infinityWhileLoop.java
@@ -0,0 +1,15 @@
+/**
+ * Infinity While Loop and do-while Loop
+ */
+public class infinityWhileLoop {
+
+    public static void main(String[] args) {
+        // boolean i = true;
+        // while (i==true) {
+            
+        // }
+        do {
+            // System.out.print("Infinity ");
+        } while (true);
+    }
+}
