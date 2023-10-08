4f36729c83bce1287cee2d0707ef3b07832a728d
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-08 19:49:32 +0530

    modified function to calculate the average of numbers entered

--- functionAverage.java
+++ functionAverage.java
@@ -2,30 +2,24 @@ import java.util.Scanner;
 
 public class functionAverage {
     public static void main(String[] args) {
+        calculateAverage(0,0);
+    }
+    
+    public static void calculateAverage(int size, int inputs) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size to find average: ");
-        int n= sc.nextInt();
+        size = sc.nextInt();
         System.out.println("Enter numbers upto the size to calculate average: ");
-        int a=sc.nextInt();
-        for(int i=0; i<n-1; i++) {
-            a = sc.nextInt();
-        }
-        int ans = calculateAverage(a, n);
-        System.out.println("The average of three number is: " + ans);
-        sc.close();
-    }
-
-    public static int calculateAverage(int size, int inputs) {
-        // Scanner sc = new Scanner(System.in);
-        int sum = inputs;
-        // for (int i = 0; i < size; i++) {
-        //     inputs = sc.nextInt();
-        // }
-        for (int i = 0; i < size-1; i++) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            inputs = sc.nextInt();
             sum = sum + inputs;
         }
-        int avg = sum / size;
-        // sc.close();
-        return avg;
+        int rem = sum % size;
+        int quiotent = sum / size;
+        System.out.println("The average of three number is: " );
+        System.out.println(quiotent + "." + rem);
+        sc.close();
+        return;
     }
 }
