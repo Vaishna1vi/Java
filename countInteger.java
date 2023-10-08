af457e80223621e55aaceaebe9da2ffe5daf6712
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-08 19:19:22 +0530

    modified program of count the total integers entered by the user.

--- countInteger.java
+++ countInteger.java
@@ -5,11 +5,11 @@ public class countInteger {
         Scanner s = new Scanner(System.in);
         // int n = s.nextInt();
         // String c = s.next();
-        count(0, null);
+        count(0, 0);
         s.close();
     }
 
-    public static void count(int input, String choice) {
+    public static void count(int input, int choice) {
         Scanner sc = new Scanner(System.in);
 
         int count = 0, pos = 0, neg = 0;
@@ -24,19 +24,18 @@ public class countInteger {
             } else {
                 pos++;
             }
-            System.out.println("Press 'Y' to continue or 'N' to exit.");
-            choice = sc.next();
-            choice.equalsIgnoreCase(choice);
-            if (choice == "y") {
+            System.out.println("Press '1' to continue or '0' to exit.");
+            choice = sc.nextInt();
+            if (choice == 1) {
                 System.out.println("Proceed.\n");
-            } else if (choice == "n") {
+            } else if (choice == 0) {
                 System.out.println("Exited.\n");
             } else {
-                System.out.println("Enter the given letters only.\nStart again.");
+                System.out.println("Enter the given numbers only.\nStart again.");
                 System.exit(i);
             }
             i++;
-        } while (i != 0);
+        } while (choice != 0);
         sc.close();
         System.out.println("The total number of zero's entered: " + count);
         System.out.println("The total number of positive integer's entered: " + pos);
