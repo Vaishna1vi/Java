1eeb3bd14f8973765c8fb2efff294f2afc8df4b4
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-10 21:30:03 +0530

    created a program to calculate the total length of all string entered in array.

---
+++ CumulativeStringArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CumulativeStringArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = s.nextInt();
+        String ar[] = new String[size];
+        System.out.println("Enter the strings in the array: ");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = s.nextLine();
+        }
+        s.close();
+        System.out.println("The array of string is: ");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i]);
+        }
+        int stringLen = 0;
+        for (int i = 0; i < ar.length; i++) {
+            stringLen += ar[i].length();
+        }
+        System.out.println("The cumulative length of the whole strings present in array is: " + stringLen);
+    }
+}
