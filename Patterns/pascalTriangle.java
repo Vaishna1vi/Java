ae6b2b2114ac1ef1304d08e1db520713845cbabe
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-11 23:07:15 +0530

    tried to make pascal's triangle pattern

--- Patterns/pascalTriangle.java
+++ Patterns/pascalTriangle.java
@@ -6,8 +6,12 @@ public class pascalTriangle {
         System.out.println("Enter the number: ");
         int n =s.nextInt();
         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 0; j <= i; j++) {
-                System.out.println();
+                int p =i+j;
+                System.out.print(p + " ");
             }
             System.out.println();
         }
