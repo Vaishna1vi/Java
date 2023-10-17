19953d4b71cd5e0d8f80d5a04b3c02a1f24d3fc9
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-17 22:59:42 +0530

    created getBit program, the concept of bit manipulation

---
+++ GetBit.java
@@ -0,0 +1,9 @@
+/**
+ * GetBit                                                                                                        
+ */
+public class GetBit {
+
+    public static void main(String[] args) {
+        
+    }
+}
