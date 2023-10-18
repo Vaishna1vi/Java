ffd2024ca8f6b5990dfbf1f3095b0cb75a3ecb14
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-18 23:28:30 +0530

    tried to make getBit program

--- GetBit.java
+++ GetBit.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 /**
  * GetBit                                                                                                        
  */
 public class GetBit {
 
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the binary number: ");
+        byte n = s.nextByte();
+        System.out.println(n);
+        s.close();
     }
 }
