9bced7845e6928a8d79196b6c54c6d48397b118e
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-20 23:10:57 +0530

    modified setBit file

--- SetBit.java
+++ SetBit.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 /**
  * SetBit
  */
 public class SetBit {
 
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        s.close();
     }
 }
