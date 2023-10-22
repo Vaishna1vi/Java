797542925451f8d870896fc6c67d367d5cf903eb
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-22 23:26:59 +0530

    Modified setBit file.

--- SetBit.java
+++ SetBit.java
@@ -7,7 +7,13 @@ public class SetBit {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number: ");
         int n = s.nextInt();
+        System.out.println("Enter the position where you want to set the bit: ");
+        int pos = s.nextInt();
+        int bitMask = 1 << pos;
+        int newNumber = bitMask | n;
+        System.out.println("The new number after set the bit on the given position will be: " + newNumber);
         s.close();
     }
 }
