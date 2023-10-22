714b373e91e9e6dbe650cdaa4c264cdd39c8e532
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-22 23:19:55 +0530

    Modified getBit file.

--- GetBit.java
+++ GetBit.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
 /**
- * GetBit                                                                                                        
+ * GetBit
  */
 public class GetBit {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the binary number: ");
-        byte n = s.nextByte();
-        System.out.println(n);
+        System.out.println("Enter the number: ");
+        int n = s.nextInt();
+        System.out.println("Enter the position where you want to check the bit: ");
+        int pos = s.nextInt();
+        int bitMask = 1 << pos;
+        System.out.println("The entered number is: " + n);
+        if ((bitMask & n) == 0) {
+            System.out.println("Bit was zero.");
+        } else {
+            System.out.println("Bit was one.");
+        }
         s.close();
     }
 }
