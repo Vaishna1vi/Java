aa46f9f0a0e4a20c0c7a3ab7f2611ba1d0c55c76
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-28 23:21:50 +0530

    Created a program to convert the decimal number into binary number.

---
+++ DecimalToBinary.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * DecimalToBinary
+ */
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the decimal number: ");
+        int n = s.nextInt();
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int rem = n % 2;
+            sb.append(rem);
+            n /= 2;
+        }
+        if (n < 0) {
+            System.out.println("Please enter the number greater than 0.");
+        } else {
+            System.out.println("Binary number of the given number is: " + sb.reverse());
+        }
+        s.close();
+    }
+}
