9db05edf9d435aa6ac8266c22592c552b7c19cb0
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-10-30 23:52:55 +0530

    modified

--- StringBuilderEx.java
+++ StringBuilderEx.java
@@ -3,27 +3,27 @@ import java.lang.StringBuilder;
 
 public class StringBuilderEx {
     public static void main(String[] args) {
-      StringBuilder ans = reverseWordSequence(null);
-      System.out.println(ans);
+        StringBuilder ans = reverseWordSequence(null);
+        System.out.println(ans);
     }
-    
+
     public static StringBuilder reverseWordSequence(String st) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the string: ");
-        st=s.nextLine();
+        st = s.nextLine();
         s.close();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < st.length(); i++) {
-            sb.insert(i,st.charAt(i));
+            sb.insert(i, st.charAt(i));
         }
-        System.out.println("The entered string is: "+ sb);
+        System.out.println("The entered string is: " + sb);
         for (int i = sb.length(); i > 0; i--) {
-            if (sb.charAt(i)=='.') {
+            if (sb.charAt(i) == '.') {
                 int index = i;
                 sb.insert(sb.charAt(index), sb.charAt(i));
             }
         }
         System.out.println("The reverse word group of string is: ");
-return sb;
+        return sb;
     }
 }
