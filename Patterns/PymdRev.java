95644c6a61b4b2dfac0a9e5c9896c70b5e684f72
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2023-11-06 23:41:35 +0530

    Update PymdRev.java

--- Patterns/PymdRev.java
+++ Patterns/PymdRev.java
@@ -1,6 +1,9 @@
 public class PymdRev {
     public static void main(String[] args) {
-        int n = 5;
+        // int n = 5;
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = s.nextInt();
         // for(int i=5;i>=0;i--) {
         for (int i = 0; i < n; i++) {
             // for(int j=0;j<i; j++) {
@@ -9,5 +12,6 @@ public class PymdRev {
             }
             System.out.println();
         }
+        s.close();
     }
 }
