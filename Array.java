35890133e6a41144fe8f67bb1eb5e3f34b822b88
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2023-11-07 22:56:14 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -15,13 +15,13 @@ public class Array {
         // arr[2] = 6;
         // arr[3] = 1;
         // arr[4] = 3;
+        System.out.println("Enter the array elements: ");
         for(i=0; i<n; i++) {
             arr[i] = s.nextInt();
-
-
-        for (i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
         }
-
+        System.out.print("The array elements are: ");
+        for (i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
     }
 }
