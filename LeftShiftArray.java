c3d652393c27efb881a275117ed7192c448a3039
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2023-11-11 23:45:34 +0530

    Update LeftShiftArray.java

--- LeftShiftArray.java
+++ LeftShiftArray.java
@@ -1,9 +1,12 @@
 /**
  * LeftShiftArray
  */
+import java.util.Scanner;
 public class LeftShiftArray {
 
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
     }
 }
